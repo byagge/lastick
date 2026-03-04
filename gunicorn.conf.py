63d7e447b00292c59dfd24ed1360994a68161c99
byagge <byagge@outlook.com>
2026-03-04 17:56:53 +0600

    fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -12,7 +12,7 @@ bind = "127.0.0.1:8000"
 backlog = 2048
 
 # Worker processes
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 3
 worker_class = "sync"
 worker_connections = 1000
 max_requests = 1000
@@ -20,7 +20,7 @@ max_requests_jitter = 50
 preload_app = True
 
 # Timeout settings
-timeout = 30
+timeout = 120
 keepalive = 2
 graceful_timeout = 30
 
@@ -32,6 +32,7 @@ accesslog = "-"
 errorlog = "-"
 loglevel = "info"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+capture_output = True 
 
 # Process management
 pidfile = "/tmp/gunicorn.pid"
